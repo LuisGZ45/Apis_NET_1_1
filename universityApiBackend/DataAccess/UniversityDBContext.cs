6f53120d46577d6a02c2f449079bdb2a8215faf5
Luis Galindo <luis.a.galindo.z@outlook.com>
2023-05-08 00:37:26 -0500

    Revert "Revert "Agregar archivos de proyecto.""
    This reverts commit 2f64059ecd541d8f8a03660fbdfd5335b4039686.

---
+++ universityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using universityApiBackend.Models.DataModels;
+
+namespace universityApiBackend.DataAccess
+{
+    public class UniversityDBContext: DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options): base(options) 
+        {
+        }
+
+        //Add DB sets (tables of ouyr data base)
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Course>? Courses { get; set; }
+    }
+}
---
+++ universityApiBackend/Migrations/20230506202054_Add Courses Table.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace universityApiBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoursesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DescriptionShort = table.Column<string>(type: "nvarchar(280)", maxLength: 280, nullable: false),
+                    DescriptionLarge = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ObjectivePublic = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Objectives = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Requirements = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
